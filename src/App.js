4ffe49f9785ac9489c7137a09c063dbc0ae50a7c
Evgeniy Kozlov <ek916569@gmail.com>
2023-02-01 12:26:31 +0500

    gfпавп

--- src/App.js
+++ src/App.js
@@ -3,15 +3,17 @@ import { Routes, Route } from "react-router";
 import Marketplace from "./pages/Marketplace";
 import Catalog from "./pages/Catalog";
 import FAQ from "./pages/FAQ";
+import CreateNFT from "./pages/CreateNFT";
 
 function App() {
     return (
         <>
             <Routes>
-                <Route exact path="/" element={<Home/>} />
-                <Route exact path="/marketplace" element={<Marketplace/>} />
-                <Route exact path="/catalog" element={<Catalog/>} />
-                <Route exact path="/faq" element={<FAQ/>} />
+                <Route exact path="/" element={<Home />} />
+                <Route exact path="/marketplace" element={<Marketplace />} />
+                <Route exact path="/catalog" element={<Catalog />} />
+                <Route exact path="/faq" element={<FAQ />} />
+                <Route exact path="/create-nft" element={<CreateNFT />} />
             </Routes>
         </>
     );
